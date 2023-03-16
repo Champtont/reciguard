d630dc3e41d7f4e3031ae4db44acfaa682cf3194
Courtney Hampton <jaye0888@gmail.com>
2023-03-16 17:04:55 +0100

    User can now get a shopping list
    Once user has selected dates on the calendar, recipes from those dates are pushed to a shopping page where ingredients are extracted and listed once.

--- src/components/views/myShoppingList/MyShoppingList.jsx
+++ src/components/views/myShoppingList/MyShoppingList.jsx
@@ -1,17 +1,68 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCurrentUser } from "../../../redux/actions";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import { useState } from "react";
 
 const MyShoppingList = () => {
+  const userShopMenus = useSelector((state) => state.user.calendar);
+  const [shoppingIngredients, setShoppingIngredients] = useState([]);
+  //loop for ingredients
+  //let shoppingIngredients = [];
+
+  const generateIngredients = () => {
+    for (let i = 0; i < userShopMenus.length; i++) {
+      console.log("1");
+      for (let j = 0; j < userShopMenus[i].recipes.length; j++) {
+        console.log("2");
+        for (
+          let n = 0;
+          n < userShopMenus[i].recipes[j].ingredients.length;
+          n++
+        ) {
+          console.log(shoppingIngredients);
+          if (
+            shoppingIngredients.includes(
+              userShopMenus[i].recipes[j].ingredients[n]
+            )
+          ) {
+            continue;
+          } else {
+            setShoppingIngredients((shoppingIngredients) => [
+              ...shoppingIngredients,
+              userShopMenus[i].recipes[j].ingredients[n],
+            ]);
+          }
+        }
+      }
+    }
+    return shoppingIngredients;
+  };
+  const stateRef = useRef();
   const dispatch = useDispatch();
+
+  stateRef.current = shoppingIngredients;
+
+  const newShopArray = stateRef.current.filter(function (elem, pos) {
+    return stateRef.current.indexOf(elem) == pos;
+  });
 
   useEffect(() => {
     dispatch(fetchCurrentUser());
-  });
+    generateIngredients();
+  }, [shoppingIngredients]);
 
   return (
     <div id="shoppingPage">
-      <h2>There is nothing here Yet</h2>
+      <h2>My Shopping List</h2>
+      {stateRef.current.length > 0 ? (
+        <ul>
+          {newShopArray.map((ingredient) => (
+            <li>{ingredient}</li>
+          ))}
+        </ul>
+      ) : (
+        <div>nothing yet</div>
+      )}
     </div>
   );
 };
